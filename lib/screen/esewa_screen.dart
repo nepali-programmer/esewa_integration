afb9c7686007fb79595e608a19de1fc9b9851b37
Sijan Joshi <132650339+joshisijan-vt@users.noreply.github.com>
2023-05-20 06:24:20 +0545

    init

---
+++ lib/screen/esewa_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class EsewaScreen extends StatelessWidget {
+  const EsewaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Esewa Integration'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(24.0),
+        children: [
+          Image.network(
+            'https://picsum.photos/id/237/200/300',
+            width: 300,
+            height: 200,
+          ),
+          Text(
+            'Pariatur occaecat elit esse quis.',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          ElevatedButton(
+            child: const Text('Pay with E-Sewa'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
